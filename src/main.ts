040ad71640bab6325ee444c2b5f825f602492a2c
Yassine Azedine <yassine.azedine0711@gmail.com>
2024-08-01 11:08:04 +0100

    fix cors policy to allwo request localhost:3000

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,12 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('trust proxy', 1); // trust first proxy
-  app.enableCors(corsConfig());
+  // app.enableCors(corsConfig());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://front-electro-store-kl77-eqxna4243-yassineazedines-projects.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.use(session(sessionConfig(MongoDBStore)));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 4000);
